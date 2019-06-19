9b93e8b4913720195f62bc7293509274f7d9149e
Ruslan <shakirovusa@gmail.com>
2019-06-19 14:21:54 +0600

    Add generic method isBetween for LocalDate and LocalTime

--- src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/DateTimeUtil.java
@@ -1,19 +1,13 @@
 package ru.javawebinar.topjava.util;
 
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTimeUtil {
     private static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
 
-    public static boolean isBetween(LocalTime lt, LocalTime startTime, LocalTime endTime) {
+    public static <T extends Comparable<? super T>> boolean isBetween(T lt, T startTime, T endTime) {
         return lt.compareTo(startTime) >= 0 && lt.compareTo(endTime) <= 0;
-    }
-
-    public static boolean isBetweenLocalDateTime(LocalDateTime ldt, LocalDateTime startDateTime,
-                                                 LocalDateTime endDateTime) {
-        return ldt.compareTo(startDateTime) >= 0 && ldt.compareTo(endDateTime) <= 0;
     }
 
     public static String toString(LocalDateTime ldt) {
