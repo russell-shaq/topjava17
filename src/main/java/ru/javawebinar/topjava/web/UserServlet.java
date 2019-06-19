81c28ad3bab51489208d5a124149ea559d73d809
Ruslan <shakirovusa@gmail.com>
2019-06-19 14:15:19 +0600

    Add logic for switching users and setting authorized userId

--- src/main/java/ru/javawebinar/topjava/web/UserServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/UserServlet.java
@@ -18,4 +18,12 @@ public class UserServlet extends HttpServlet {
         log.debug("forward to users");
         request.getRequestDispatcher("/users.jsp").forward(request, response);
     }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        int userId = Integer.parseInt(req.getParameter("userId"));
+        log.info("set userId {}", userId);
+        SecurityUtil.setUserId(userId);
+        resp.sendRedirect("meals");
+    }
 }
