e8a32047e357adb28ab697540c161b69a920dacf
Ruslan <shakirovusa@gmail.com>
2019-06-19 14:32:27 +0600

    Add userId variable and setId method

--- src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
+++ src/main/java/ru/javawebinar/topjava/web/SecurityUtil.java
@@ -3,9 +3,14 @@ package ru.javawebinar.topjava.web;
 import static ru.javawebinar.topjava.util.MealsUtil.DEFAULT_CALORIES_PER_DAY;
 
 public class SecurityUtil {
+    public static int USER_ID = 1;
+
+    public static void setUserId(int userId) {
+        USER_ID = userId;
+    }
 
     public static int authUserId() {
-        return 1;
+        return USER_ID;
     }
 
     public static int authUserCaloriesPerDay() {
