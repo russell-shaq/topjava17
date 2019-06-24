4a29659a3088426fce11f1912ffdeb512df73f18
Ruslan <shakirovusa@gmail.com>
2019-06-24 16:32:05 +0600

    Implement JdbcMealRepository via spring jdbc template

--- src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
+++ src/main/java/ru/javawebinar/topjava/repository/jdbc/JdbcMealRepositoryImpl.java
@@ -1,37 +1,96 @@
 package ru.javawebinar.topjava.repository.jdbc;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
+import javax.sql.DataSource;
+import java.sql.Timestamp;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class JdbcMealRepositoryImpl implements MealRepository {
 
+    private final JdbcTemplate jdbcTemplate;
+    private final NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+    private final SimpleJdbcInsert insertMeal;
+    private static final BeanPropertyRowMapper<Meal> ROW_MAPPER =
+            new BeanPropertyRowMapper<>(Meal.class);
+
+    @Autowired
+    public JdbcMealRepositoryImpl(DataSource dataSource, JdbcTemplate jdbcTemplate,
+                                  NamedParameterJdbcTemplate namedParameterJdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+        this.namedParameterJdbcTemplate = namedParameterJdbcTemplate;
+        this.insertMeal = new SimpleJdbcInsert(dataSource)
+                .withTableName("meals")
+                .usingGeneratedKeyColumns("id");
+        ;
+    }
+
+
     @Override
     public Meal save(Meal meal, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("id", meal.getId());
+        map.addValue("dateTime", meal.getDateTime());
+        map.addValue("description", meal.getDescription());
+        map.addValue("calories", meal.getCalories());
+        map.addValue("userId", userId);
+        if (meal.isNew()) {
+            Number newKey = insertMeal.executeAndReturnKey(map);
+            meal.setId(newKey.intValue());
+        } else {
+            int update = namedParameterJdbcTemplate.update("UPDATE meals SET date_time = :dateTime, " +
+                    "description = :description, calories = :calories WHERE id = :id AND" +
+                    " user_id = :userId", map);
+            if (update == 0) {
+                return null;
+            }
+        }
+        return meal;
     }
 
     @Override
     public boolean delete(int id, int userId) {
-        return false;
+        return jdbcTemplate.update("DELETE FROM meals WHERE id = ? AND user_id = ?", id, userId) != 0;
     }
 
     @Override
     public Meal get(int id, int userId) {
-        return null;
+        MapSqlParameterSource map = new MapSqlParameterSource();
+        map.addValue("id", id);
+        map.addValue("userId", userId);
+        List<Meal> meals = namedParameterJdbcTemplate
+                .query("SELECT * FROM meals WHERE id = :id AND user_id = :userId",
+                        map, ROW_MAPPER);
+        return DataAccessUtils.singleResult(meals);
     }
 
     @Override
     public List<Meal> getAll(int userId) {
-        return null;
+        return jdbcTemplate.query("SELECT * FROM meals where user_id = ? ORDER BY date_time DESC",
+                ROW_MAPPER, userId);
     }
 
     @Override
     public List<Meal> getBetween(LocalDateTime startDate, LocalDateTime endDate, int userId) {
-        return null;
+        Map<String, Object> parameterSource = new HashMap<>();
+        parameterSource.put("startDate", Timestamp.valueOf(startDate));
+        parameterSource.put("endDate", Timestamp.valueOf(endDate));
+        parameterSource.put("userId", userId);
+        return namedParameterJdbcTemplate.query("SELECT * FROM meals WHERE date_time >= :startDate " +
+                        "AND date_time <= :endDate AND user_id = :userId ORDER BY date_time DESC",
+                parameterSource, ROW_MAPPER);
     }
 }
