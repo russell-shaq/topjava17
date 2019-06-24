c5b6ea285641e756bc24d87f84c94672bd84465c
Ruslan <shakirovusa@gmail.com>
2019-06-24 16:19:17 +0600

    Add create meals table

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -23,3 +24,15 @@ CREATE TABLE user_roles
   CONSTRAINT user_roles_idx UNIQUE (user_id, role),
   FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
+
+CREATE TABLE meals
+(
+    id INT PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time TIMESTAMP WITHOUT TIME ZONE,
+    description VARCHAR NOT NULL,
+    calories INT NOT NULL,
+    user_id INT NOT NULL
+);
+--
+-- CREATE UNIQUE INDEX meals_unique_date_time_index ON meals(date_time);
+-- CREATE INDEX meals_user_id_index ON meals (user_id);
