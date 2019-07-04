5ac3d443b2062271cb04f7e01fe3193c137d4f09
Ruslan <shakirovusa@gmail.com>
2019-07-04 14:16:08 +0600

    Add NamedQueries for Meal entity

--- src/main/java/ru/javawebinar/topjava/model/Meal.java
+++ src/main/java/ru/javawebinar/topjava/model/Meal.java
@@ -1,16 +1,42 @@
 package ru.javawebinar.topjava.model;
 
-import javax.persistence.FetchType;
-import javax.persistence.ManyToOne;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@NamedQueries({
+        @NamedQuery(name = Meal.DELETE_BY_ID_USER_ID, query = "DELETE FROM Meal m WHERE " +
+                "m.id = :id AND m.user.id = :userId"),
+        @NamedQuery(name = Meal.GET_BY_ID_USER_ID, query = "SELECT m FROM Meal m WHERE " +
+                "m.id = :id AND m.user.id = :userId"),
+        @NamedQuery(name = Meal.GET_ALL_BY_USER_ID, query = "SELECT m FROM Meal m WHERE " +
+                "m.user.id = :userId ORDER BY m.dateTime DESC"),
+        @NamedQuery(name = Meal.GET_BETWEEN_DATES_USER_ID, query = "SELECT m FROM Meal m " +
+                "WHERE m.dateTime BETWEEN :startDate AND :endDate AND m.user.id = :userId " +
+                "ORDER BY m.dateTime DESC")
+})
+@Entity
+@Table(name = "meals")
 public class Meal extends AbstractBaseEntity {
+    public static final String DELETE_BY_ID_USER_ID = "Meal.deleteByIdAndUserId";
+    public static final String GET_BY_ID_USER_ID = "Meal.getByIdAndUserId";
+    public static final String GET_ALL_BY_USER_ID = "Meal.getAllByUserId";
+    public static final String GET_BETWEEN_DATES_USER_ID = "Meal.getBetweenDatesAndUserId";
+    @NotNull
+    @Column(name = "date_time")
     private LocalDateTime dateTime;
 
+    @NotBlank
+    @Size(max = 100)
     private String description;
 
+    @Range(min = 10, max = 10000)
     private int calories;
 
     @ManyToOne(fetch = FetchType.LAZY)
